2fe4bda4c2837f93dc39d78a2e19ef04cb3a47f8
Jair Pereira Junior <pereira-junior.ua.ws@alumni.tsukuba.ac.jp>
2026-03-13 12:01:01 +0900

    Update main.py

--- configs_editor/main.py
+++ configs_editor/main.py
@@ -168,7 +168,7 @@ def run(config_filepath, basedir):
         try:
             assert int(analyses[name]['runtime']['output'].returncode)==0
         except Exception:
-            logger.exception("Pipeline Crashed while running {name.upper()}!!".upper())
+            logger.exception(f"Pipeline Crashed while running {name.upper()}!!".upper())
             logger.exception("+ Current analysis failed to run. Please, check its log file.")
             raise
         logger.info(f"+ Output: {analyses[name]['runtime']['output'].stdout}")
