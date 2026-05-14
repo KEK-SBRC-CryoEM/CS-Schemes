8b5533453ffa5acf1f0f52a4cfad248b9092f818
Jair Pereira Junior <pereira-junior.ua.ws@alumni.tsukuba.ac.jp>
2026-05-14 11:54:40 +0900

    Update css_parameters.py

--- configs_editor/css_parameters.py
+++ configs_editor/css_parameters.py
@@ -83,7 +83,7 @@ class CSSParameters:
 
     @property
     def SS_comm_lbin_angpix(self):
-        return self.EM_mics_apix / self.large_binning_factor
+        return self.large_binning_factor / self.EM_mics_apix
 
     @property
     def SS_comm_lbin_ref3d_path(self):
@@ -103,7 +103,7 @@ class CSSParameters:
 
     @property
     def SS_comm_mbin_angpix(self):
-        return self.EM_mics_apix * self.medium_binning_factor
+        return self.medium_binning_factor / self.EM_mics_apix
 
     ##### 030_GTF_Create_Stack #####
     @property
